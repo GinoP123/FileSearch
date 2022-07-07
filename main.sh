7e3c62c976d3d0590ebf006cdc40c870a8a153d5
Gino Prasad <ginoprasad@gmail.com>
2022-07-07 09:39:48 -0700

    Cleaned Code

--- main.sh
+++ main.sh
@@ -1,26 +1,19 @@
 #!/bin/zsh
 
-cwd="$PWD"
-
 if [[ "$SHELL" == *zsh ]]; then
-	cd "$(dirname "$0")"
+	dir="$(dirname "$0")"
 else
-	cd "$(dirname "${BASH_SOURCE[0]}")"
+	dir="$(dirname "${BASH_SOURCE[0]}")"
 fi
 
-./file_search.py search "$*"
+"$dir/file_search.py" search "$*"
 
 if [[ $? == "0" ]]; then
-	selected_path="$(./file_search.py get_output)"
-
+	selected_path="$("$dir/file_search.py" get_output)"
 	if [[ -d "$selected_path" ]]; then
 		cd "$selected_path"
 	else
-		./open "$selected_path"
+		"$dir/open" "$selected_path"
 	fi
-else
-	cd "$cwd"
 fi
-
-unset cwd
 
