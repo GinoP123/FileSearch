52daed89b4fc4f620e8cd6c39cc3aa742e0154c9
Gino Prasad <ginoprasad@gmail.com>
2022-07-20 17:39:36 -0700

    Remove Unnecessary source dir check

--- delete_path.sh
+++ delete_path.sh
@@ -1,18 +1,11 @@
 #!/bin/zsh
 
-#!/bin/zsh
-
-if [[ "$SHELL" == *zsh ]]; then
-	dir="$(dirname "$0")"
-else
-	dir="$(dirname "${BASH_SOURCE[0]}")"
-fi
-
-"$dir/file_search.py" search "$*"
+fs_script="$(dirname "$0")/file_search.py"
+"$fs_script" search "$*"
 
 if [[ $? == "0" ]]; then
-	selected_path="$("$dir/file_search.py" get_output)"
-	"$dir/file_search.py" "delete" "$selected_path"
+	selected_path="$("$fs_script" get_output)"
+	"$fs_script" "delete" "$selected_path"
 	echo -e "\tDELETED $selected_path\n"
 fi
 
